2120a39e383d447ecc95ab7fa78abf443f78fe98
Teemo <Teemo@Teemo-PC>
2015-06-13 16:02:43 +0300

    Currency ticker added

--- include/scripts.js
+++ include/scripts.js
@@ -5,4 +5,14 @@ $(document).ready(function () {
         $("#menu-options").slideToggle();
     });
 
+    //currencies ticker fix
+    setTimeout(function(){
+        var source = $("#currency-exchange iframe").attr("src");
+        var currencyWidth = $("#currency-exchange").width();
+        currencyWidth = currencyWidth - 2;
+        
+        source = source.replace(".php?w=700", ".php?w="+currencyWidth);
+        $("#currency-exchange iframe").attr("src", source).delay(350).fadeIn(500);        
+    }, 500);
+
 });
