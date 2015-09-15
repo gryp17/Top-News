ff836b517e92562031191066291dd29c8dbafde6
plamen <plamen@plamen-pc>
2015-09-15 17:21:57 +0300

    Test

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
 require_once 'app/init.php';
-#in order to use .htaccess file change the AllowOverride to "All" in the apache config
+#in order to use .htaccess file change the AllowOverride to "All" in the apache config  
