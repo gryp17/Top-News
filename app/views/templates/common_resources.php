3591818638e5318a097f87848380b28f77a93865
plamen <plamen@plamen-pc>
2015-11-11 13:52:57 +0200

    Migrating CSS to SASS

--- app/views/templates/common_resources.php
+++ app/views/templates/common_resources.php
@@ -1,16 +1,16 @@
          
         <!-- CSS -->
         <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700,600' rel='stylesheet' type='text/css'>
-        <link href="include/bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css"/>
-		<link href="include/jqueryui/jquery-ui.css" rel="stylesheet" type="text/css"/>
-        <link href="include/style.css" rel="stylesheet" type="text/css"/>
-        <link href="include/queries.css" rel="stylesheet" type="text/css"/>
+        <link href="include/css/bootstrap/bootstrap.css" rel="stylesheet" type="text/css"/>
+		<link href="include/css/jqueryui/jquery-ui.css" rel="stylesheet" type="text/css"/>
+        <link href="include/css/style.css" rel="stylesheet" type="text/css"/>
+        <link href="include/css/queries.css" rel="stylesheet" type="text/css"/>
         <!-- JS -->
-        <script type="text/javascript" src="include/jquery-2.1.4.min.js"></script>
-		<script type="text/javascript" src="include/jqueryui/jquery-ui.min.js"></script>
-        <script type="text/javascript" src="include/bootstrap/js/bootstrap.js"></script>
-        <script type="text/javascript" src="include/angular.min.js"></script>
-        <script type="text/javascript" src="include/angular-route.min.js"></script>
+        <script type="text/javascript" src="include/js/jquery/jquery-2.1.4.min.js"></script>
+		<script type="text/javascript" src="include/js/jqueryui/jquery-ui.min.js"></script>
+        <script type="text/javascript" src="include/js/bootstrap/bootstrap.js"></script>
+        <script type="text/javascript" src="include/js/angular/angular.min.js"></script>
+        <script type="text/javascript" src="include/js/angular/angular-route.min.js"></script>
         <script type="text/javascript" src="app/app.js"></script>
 		<script type="text/javascript" src="app/filters/custom_filters.js"></script>
 		<script type="text/javascript" src="app/services/search_service.js"></script>
@@ -19,5 +19,5 @@
         <script type="text/javascript" src="app/controllers/articles.js"></script>
         <script type="text/javascript" src="app/controllers/archive.js"></script>
         <script type="text/javascript" src="app/controllers/search.js"></script>
-        <script type="text/javascript" src="include/scripts.js"></script>
+        <script type="text/javascript" src="include/js/scripts.js"></script>
 
