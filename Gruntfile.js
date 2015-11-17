d5abd55a82d1078b8c4f512a821307c88fa3697d
plamen <plamen@plamen-pc>
2015-11-17 16:20:53 +0200

    Added bower + grunt deploying.

---
+++ Gruntfile.js
@@ -0,0 +1,102 @@
+module.exports = function (grunt) {
+
+	//load modules
+	grunt.loadNpmTasks("grunt-bower-install-simple");
+	grunt.loadNpmTasks("grunt-contrib-concat");
+	grunt.loadNpmTasks("grunt-contrib-copy");
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+
+	//project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		//bower install
+		'bower-install-simple': {
+			options: {
+				color: true,
+				directory: "bower_components"
+			},
+			dev: {
+				options: {
+					production: false
+				}
+			}
+		},
+		//concat
+		concat: {
+			js_libs: {
+				files: {
+					'include/js/lib.js': [
+						'bower_components/jquery/dist/jquery.min.js',
+						'bower_components/jquery-ui/jquery-ui.min.js',
+						'bower_components/bootstrap/dist/js/bootstrap.min.js',
+						'bower_components/angular/angular.min.js',
+						'bower_components/angular-route/angular-route.min.js'
+					]
+				}
+			},
+			css_libs: {
+				files: {
+					'include/css/lib.css': [
+						'bower_components/jquery-ui/themes/redmond/jquery-ui.min.css',
+						'bower_components/bootstrap/dist/css/bootstrap.min.css',
+					]
+				}
+			}
+		},
+		//copy
+		copy: {
+			'jquery-ui': {
+				files: [
+					{
+						dest: 'include/css/images/',
+						src: 'bower_components/jquery-ui/themes/redmond/images/*',
+						flatten: true,
+						expand: true
+					}
+				]
+			},
+			bootstrap: {
+				files: [
+					{
+						dest: 'include/fonts/',
+						src: 'bower_components/bootstrap/dist/fonts/*',
+						flatten: true,
+						expand: true
+					}
+				]
+			}
+		},
+		//uglify
+		uglify: {
+			options: {
+				mangle: false
+			},
+			my_target: {
+				files: {
+					'include/js/top-news.js': [
+						'app/app.js',
+						'app/services/API_service.js',
+						'app/services/search_service.js',
+						'app/filters/custom_filters.js',
+						'app/controllers/home.js',
+						'app/controllers/archive.js',
+						'app/controllers/articles.js',
+						'app/controllers/search.js'
+					]
+				}
+			}
+		}
+	});
+
+
+	//register get-dependencies task
+	grunt.registerTask('get-dependencies', [
+		'bower-install-simple',
+		'concat',
+		'copy',
+		'uglify'
+	]);
+
+
+};
+
--- app/views/templates/common_resources.php
+++ app/views/templates/common_resources.php
@@ -1,23 +1,11 @@
          
         <!-- CSS -->
         <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700,600' rel='stylesheet' type='text/css'>
-        <link href="include/css/bootstrap/bootstrap.css" rel="stylesheet" type="text/css"/>
-		<link href="include/css/jqueryui/jquery-ui.css" rel="stylesheet" type="text/css"/>
+		<link href="include/css/lib.css" rel="stylesheet" type="text/css"/>
         <link href="include/css/main.css" rel="stylesheet" type="text/css"/>
-       <!-- <link href="include/css/queries.css" rel="stylesheet" type="text/css"/> -->
-        <!-- JS -->
-        <script type="text/javascript" src="include/js/jquery/jquery-2.1.4.min.js"></script>
-		<script type="text/javascript" src="include/js/jqueryui/jquery-ui.min.js"></script>
-        <script type="text/javascript" src="include/js/bootstrap/bootstrap.js"></script>
-        <script type="text/javascript" src="include/js/angular/angular.min.js"></script>
-        <script type="text/javascript" src="include/js/angular/angular-route.min.js"></script>
-        <script type="text/javascript" src="app/app.js"></script>
-		<script type="text/javascript" src="app/filters/custom_filters.js"></script>
-		<script type="text/javascript" src="app/services/search_service.js"></script>
-		<script type="text/javascript" src="app/services/API_service.js"></script>
-        <script type="text/javascript" src="app/controllers/home.js"></script>
-        <script type="text/javascript" src="app/controllers/articles.js"></script>
-        <script type="text/javascript" src="app/controllers/archive.js"></script>
-        <script type="text/javascript" src="app/controllers/search.js"></script>
+        
+		<!-- JS -->
+		<script type="text/javascript" src="include/js/lib.js"></script>
+		<script type="text/javascript" src="include/js/top-news.js"></script>
         <script type="text/javascript" src="include/js/scripts.js"></script>
 
