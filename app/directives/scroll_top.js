ab81b39b6d4b3a4211a40eecef2af36c01b82dd9
plamen <plamen@plamen-pc>
2015-12-14 15:45:50 +0200

    Scroll to top directive refactoring

--- app/directives/scroll_top.js
+++ app/directives/scroll_top.js
@@ -1,23 +1,41 @@
-app.directive('scrollTop', function () {
+app.directive('scrollTop', function ($timeout, $window, $document) {
 	return {
 		restrict: 'C',
 		replace: true,
 		templateUrl: 'app/views/directives/scroll_top.php',
 		link: function (scope, element, attr) {
 
+			//if the width is bigger than 768px
+			if (angular.element($window).width() >= 768) {
+				//apply the scroll handler
+				angular.element($window).on('scroll', function () {
+					var height = angular.element($window).height();
+					var top = angular.element($window).scrollTop();
+
+					if (top >= height) {
+						element.fadeIn();
+					} else {
+						element.fadeOut();
+					}
+				});
+			}
+			
+			/*
 			//watch for document.height changes
 			scope.$watch(function () {
-				return $(document).height();
+				return angular.element($document).height();
 			}, function onHeightChange(newValue, oldValue) {
 
-				//if the height has changed and the width is bigger than 768px
-				if (newValue != oldValue && $(document).width() >= 768) {
-					//apply the scroll handler
-					$(window).scroll(function () {
-						var height = $(window).height();
-						var top = $(this).scrollTop();
+				var width = angular.element($window).width();
 
-						if (top >= height + height / 2) {
+				//if the height has changed and the width is bigger than 768px
+				if (newValue != oldValue && width >= 768) {
+					//apply the scroll handler
+					angular.element($window).on('scroll', function (){
+						var height = angular.element($window).height();
+						var top = angular.element($window).scrollTop();
+
+						if (top >= height) {
 							element.fadeIn();
 						} else {
 							element.fadeOut();
@@ -25,13 +43,13 @@ app.directive('scrollTop', function () {
 					});
 				}
 
-			});
-
+			});*/
+			
 			//apply the click handler
 			element.click(function () {
 				$("html, body").animate({scrollTop: 0}, 1000);
 			});
 
 		}
-	}
+	};
 });
